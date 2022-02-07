64597db433732c001758a73cebd0452ff3f1e412
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-02-06 16:26:09 -0800

    Use `withr` in tests

--- tests/testthat/test-and.R
+++ tests/testthat/test-and.R
@@ -1,127 +1,118 @@
-remotes::install_local(here::here(), upgrade = "never")
-
-with_language <- function(lang, expr) {
-  callr::r(
-    as.function(list(substitute(expr))),
-    env = c("LANGUAGE" = lang)
-  )
-}
-
 test_that("and", {
   expect_equal(
-    with_language("en_US", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("en_US", and(c("Euridice", "Orpheus"))),
     "Euridice and Orpheus"
   )
 
   expect_equal(
-    with_language("en_GB", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("en_GB", and(c("Euridice", "Orpheus"))),
     "Euridice and Orpheus"
   )
 
   expect_equal(
-    with_language("es", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("es", and(c("Euridice", "Orpheus"))),
     "Euridice y Orpheus"
   )
 
   expect_equal(
-    with_language("fr", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("fr", and(c("Euridice", "Orpheus"))),
     "Euridice et Orpheus"
   )
 
   expect_equal(
-    with_language("pt", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("pt", and(c("Euridice", "Orpheus"))),
     "Euridice e Orpheus"
   )
 
   expect_equal(
-    with_language("pt_BR", and::and(c("Euridice", "Orpheus"))),
+    withr::with_language("pt_BR", and(c("Euridice", "Orpheus"))),
     "Euridice e Orpheus"
   )
 
   expect_equal(
-    with_language("en_US", and::and(c("James", "Betty", "Inez"))),
+    withr::with_language("en_US", and(c("James", "Betty", "Inez"))),
     "James, Betty, and Inez"
   )
 
   expect_equal(
-    with_language("en_GB", and::and(c("James", "Betty", "Inez"))),
+    withr::with_language("en_GB", and(c("James", "Betty", "Inez"))),
     "James, Betty and Inez"
   )
 
   expect_equal(
-    with_language("es", and::and(c("James", "Betty", "Inez"))),
+    withr::with_language("es", and(c("James", "Betty", "Inez"))),
     "James, Betty e Inez"
   )
 
   expect_equal(
-    with_language("fr", and::and(c("James", "Betty", "Inez"))),
+    withr::with_language("fr", and(c("James", "Betty", "Inez"))),
     "James, Betty et Inez"
   )
 
   expect_equal(
-    with_language("pt", and::and(c("James", "Betty", "Inez"))),
+    withr::with_language("pt", and(c("James", "Betty", "Inez"))),
     "James, Betty e Inez"
   )
 
   expect_equal(
-    with_language("pt_BR", and::and(c("Euridice", "Orpheus"))),
-    "Euridice e Orpheus"
+    withr::with_language("pt_BR", and(c("James", "Betty", "Inez"))),
+    "James, Betty e Inez"
   )
 })
 
 test_that("or", {
   expect_equal(
-    with_language("en_US", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("en_US", or(c("Euridice", "Orpheus"))),
     "Euridice or Orpheus"
   )
 
   expect_equal(
-    with_language("en_GB", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("en_GB", or(c("Euridice", "Orpheus"))),
     "Euridice or Orpheus"
   )
 
   expect_equal(
-    with_language("es", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("es", or(c("Euridice", "Orpheus"))),
     "Euridice u Orpheus"
   )
 
   expect_equal(
-    with_language("fr", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("fr", or(c("Euridice", "Orpheus"))),
     "Euridice ou Orpheus"
   )
 
   expect_equal(
-    with_language("pt", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("pt", or(c("Euridice", "Orpheus"))),
     "Euridice ou Orpheus"
   )
 
   expect_equal(
-    with_language("pt_BR", and::or(c("Euridice", "Orpheus"))),
+    withr::with_language("pt_BR", or(c("Euridice", "Orpheus"))),
     "Euridice ou Orpheus"
   )
 
   expect_equal(
-    with_language("en_US", and::or(c("James", "Betty", "Inez"))),
+    withr::with_language("en_US", or(c("James", "Betty", "Inez"))),
     "James, Betty, or Inez"
   )
 
   expect_equal(
-    with_language("en_GB", and::or(c("James", "Betty", "Inez"))),
+    withr::with_language("en_GB", or(c("James", "Betty", "Inez"))),
     "James, Betty or Inez"
   )
 
   expect_equal(
-    with_language("es", and::or(c("James", "Betty", "Inez"))),
+    withr::with_language("es", or(c("James", "Betty", "Inez"))),
     "James, Betty o Inez"
   )
 
   expect_equal(
-    with_language("fr", and::or(c("James", "Betty", "Inez"))),
+    withr::with_language("fr", or(c("James", "Betty", "Inez"))),
     "James, Betty ou Inez"
   )
 
   expect_equal(
-    with_language("pt", and::or(c("James", "Betty", "Inez"))),
+    withr::with_language("pt", or(c("James", "Betty", "Inez"))),
     "James, Betty ou Inez"
   )
 })
