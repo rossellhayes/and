396d3987feb21348420117afca4a81246416807c
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-10-21 12:19:00 -0700

    chore: set `nocov` on `release_questions()`

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,4 @@ tag <- function(...) {
   return("")
 }
 
-release_questions <- function() {
-  "Did you run `urlchecker::url_check()`?"
-}
+release_questions <- function() "Did you run `urlchecker::url_check()`?" #nocov
