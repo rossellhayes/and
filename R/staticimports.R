7a4d3792baeab68568b5770e2062403f644bcf5d
Alex Rossell Hayes <alexander@rossellhayes.com>
2023-06-13 14:23:49 -0700

    chore: update staticimports

--- R/staticimports.R
+++ R/staticimports.R
@@ -9,18 +9,69 @@ is_windows <- function() .Platform$OS.type == "windows"
 # Imported from pkg:stringstatic
 # ======================================================================
 
-`%||%` <- function(lhs, rhs) {
-	if (length(lhs) == 0) return(rhs)
-	lhs
-}
-
+#' Remove matched patterns in a string
+#'
+#' Dependency-free drop-in alternative for `stringr::str_remove_all()`.
+#'
+#' @source Adapted from the [stringr](https://stringr.tidyverse.org/) package.
+#'
+#' @param string Input vector.
+#'   Either a character vector, or something coercible to one.
+#'
+#' @param pattern Pattern to look for.
+#'
+#'   The default interpretation is a regular expression,
+#'   as described in [base::regex].
+#'   Control options with [regex()].
+#'
+#'   Match a fixed string (i.e. by comparing only bytes), using [fixed()].
+#'   This is fast, but approximate.
+#'
+#' @return A character vector.
+#' @noRd
 str_remove_all <- function(string, pattern) {
-	str_replace_all(string, pattern, replacement = "")
+	if (length(string) == 0 || length(pattern) == 0) return(character(0))
+	is_fixed <- inherits(pattern, "stringr_fixed")
+	Vectorize(gsub, c("pattern", "x"), USE.NAMES = FALSE)(
+		pattern, replacement = "", x = string, perl = !is_fixed, fixed = is_fixed
+	)
 }
 
-str_replace_all <- function(string, pattern, replacement, fixed = FALSE) {
-	is_fixed <- inherits(pattern, "fixed")
-	ignore.case <- attr(pattern, "options")$case_insensitive %||% FALSE
+#' Replace matched patterns in a string
+#'
+#' Dependency-free drop-in alternative for `stringr::str_replace_all()`.
+#'
+#' @source Adapted from the [stringr](https://stringr.tidyverse.org/) package.
+#'
+#' @param string Input vector.
+#'   Either a character vector, or something coercible to one.
+#'
+#' @param pattern Pattern to look for.
+#'
+#'   The default interpretation is a regular expression,
+#'   as described in [base::regex].
+#'   Control options with [regex()].
+#'
+#'   Match a fixed string (i.e. by comparing only bytes), using [fixed()].
+#'   This is fast, but approximate.
+#'
+#' @param replacement A character vector of replacements.
+#'   Should be either length one, or the same length as `string` or `pattern`.
+#'   References of the form `\1`, `\2`, etc. will be replaced with the contents
+#'   of the respective matched group (created by `()`).
+#'
+#'   To perform multiple replacements in each element of `string`,
+#'   pass a named vector `(c(pattern1 = replacement1))` to `str_replace_all()`.
+#'
+#'   To replace the complete string with `NA`,
+#'   use `replacement = NA_character_`.
+#'
+#'   Using a function for `replacement` is not yet supported.
+#'
+#' @return A character vector.
+#' @noRd
+str_replace_all <- function(string, pattern, replacement) {
+	is_fixed <- inherits(pattern, "stringr_fixed")
 
 	if (!is.null(names(pattern))) {
 		for (i in seq_along(pattern)) {
@@ -28,21 +79,18 @@ str_replace_all <- function(string, pattern, replacement, fixed = FALSE) {
 				pattern = names(pattern)[[i]],
 				replacement = pattern[[i]],
 				x = string,
-				ignore.case = ignore.case,
 				perl = !is_fixed,
 				fixed = is_fixed
 			)
 		}
-
 		return(string)
 	}
 
-	gsub(
-		pattern,
-		replacement,
-		x = string,
-		ignore.case = ignore.case,
-		perl = !is_fixed,
-		fixed = is_fixed
+	if (length(string) == 0 || length(pattern) == 0 || length(replacement) == 0) {
+		return(character(0))
+	}
+
+	Vectorize(gsub, c("pattern", "replacement", "x"), USE.NAMES = FALSE)(
+		pattern, replacement, x = string, perl = !is_fixed, fixed = is_fixed
 	)
 }
